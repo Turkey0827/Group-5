f1a21973e26a4ecf2226cb1bbd7e28ba8b91b3f7
yu759 <154353739+yu759@users.noreply.github.com>
2024-10-13 17:31:56 +0100

    Update SessionExtensions.cs

--- SessionExtensions.cs
+++ SessionExtensions.cs
@@ -1,6 +1,16 @@
 ﻿namespace WebApplication1.Extension
 {
-    public class SessionExtensions
+   public static class SessionExtensions
+{
+    public static void SetObjectAsJson(this ISession session, string key, object value)
     {
+        session.SetString(key, JsonSerializer.Serialize(value));
     }
+
+    public static T GetObjectFromJson<T>(this ISession session, string key)
+    {
+        var value = session.GetString(key);
+        return value == null ? default : JsonSerializer.Deserialize<T>(value);
+    }
+}
 }
