1ad7919162c22a7ef93bf2538b9eed8a14bf3cdf
Turkey0827 <1483881208@qq.com>
2024-10-24 22:22:25 +0100

    Add files via upload

---
+++ LoginController.cs
@@ -0,0 +1,19 @@
+﻿namespace Group5_Website.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    namespace Group5_Website.Controllers
+    {
+        public class LoginController : Controller
+        {
+            public IActionResult Login()
+            {
+                return View(); // 这会自动查找 /Views/Login/Index.cshtml
+            }
+            public IActionResult Register()
+            {
+                return View(); // 
+            }
+        }
+    }
+}
