e3746487a1500a1d26af5ec24cdf17c93d201c1b
yu759 <154353739+yu759@users.noreply.github.com>
2024-10-24 23:38:13 +0100

    Add files via upload

--- LoginController.cs
+++ LoginController.cs
@@ -1,12 +1,44 @@
-﻿namespace Group5_Website.Controllers
-{
-    using Microsoft.AspNetCore.Mvc;
+﻿using Group5_Website.Data;
+using Microsoft.AspNetCore.Mvc;
+using Group5_Website.Models;
+using System.Linq;
 
     namespace Group5_Website.Controllers
     {
         public class LoginController : Controller
         {
-            public IActionResult Login()
+        private readonly AppDbContext _db;
+        public LoginController(AppDbContext db)
+        {
+            _db = db;
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = _db.Users.FirstOrDefault(u => u.Name == model.Name && u.Password == model.Password);
+                if (user != null)
+                {//登录成功，保存会话
+                    HttpContext.Session.SetString("User", user.Name);
+                    return RedirectToAction("Index", "Home");
+                    
+
+                }
+                else
+                {
+                    ModelState.AddModelError("", "User Name or Password is Wrong");
+                }
+            }
+            return View(model);
+        }
+
+        public IActionResult Login()
             {
                 return View(); // 这会自动查找 /Views/Login/Index.cshtml
             }
@@ -16,4 +48,4 @@
             }
         }
     }
-}
+
